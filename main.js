292604ac8617ebe85e13beecc971d1f02d68f3f5
vaishnavipanwar <88323011+vaishnavipanwar@users.noreply.github.com>
2022-06-30 20:24:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier= ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/EABx5ZEHr/model.json" , modelReady );
+}
+
+function modelReady(){
+    classifier.classify( gotResult);
+}
+
+function gotResult( error , results){
+    console.log("Got Result")
+}
